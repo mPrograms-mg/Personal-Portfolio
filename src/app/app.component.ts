c5b2416aa62e9232b5767bbdab27bff611ab6182
mPrograms-mg <maheshkshirsagar128@gmail.com>
2023-08-05 16:28:52 +0530

    new update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,55 @@
-import { DOCUMENT, ViewportScroller } from '@angular/common';
-import { Component, ElementRef, ViewChild, inject } from '@angular/core';
-import { Observable, fromEvent, map } from 'rxjs';
+import { DOCUMENT, ViewportScroller } from "@angular/common";
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+  inject,
+} from "@angular/core";
+
+import { Observable, fromEvent, map } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'mPrograms-mg';
+export class AppComponent implements OnInit {
+  title = "mPrograms-mg";
 
-  @ViewChild('navbarToggler') navbarToggler: ElementRef;
+  activeNavItem: string = "home"; // Set the default active navigation item
+
+  @HostListener("window:scroll", []) onScroll(): void {
+    const homeElement = document.getElementById("home");
+    const aboutElement = document.getElementById("about");
+    const skillElement = this.document.getElementById("skill");
+    const workElement = this.document.getElementById("work");
+    const contectElement = this.document.getElementById("contact");
+
+    if (this.isElementInViewport(homeElement)) {
+      this.activeNavItem = "home";
+    } else if (this.isElementInViewport(aboutElement)) {
+      this.activeNavItem = "about";
+    } else if (this.isElementInViewport(skillElement)) {
+      this.activeNavItem = "skill";
+    } else if (this.isElementInViewport(workElement)) {
+      this.activeNavItem = "work";
+    } else if (this.isElementInViewport(contectElement)) {
+      this.activeNavItem = "contact";
+    }
+  }
+
+  isElementInViewport(element: HTMLElement): boolean {
+    const rect = element.getBoundingClientRect();
+    return rect.top <= 100 && rect.bottom >= 100;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @ViewChild("navbarToggler") navbarToggler: ElementRef;
 
   navBarTogglerIsVisible() {
     return this.navbarToggler.nativeElement.offsetParent !== null;
@@ -27,7 +66,7 @@ export class AppComponent {
 
   readonly showScroll$: Observable<boolean> = fromEvent(
     this.document,
-    'scroll'
+    "scroll"
   ).pipe(map(() => this.viewport.getScrollPosition()?.[1] > 0));
 
   onScrollToTop(): void {
